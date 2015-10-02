909a64e255feae668b5e180e960bc707c17f3c5d
Francesco Maida <francesco.maida@gmail.com>
2015-10-02 21:39:19 +0200

    Mi ero dimenticato di sostituire gli spazi con i tab nel file principale, maledetto PEP8...

--- __main__.py
+++ __main__.py
@@ -8,8 +8,8 @@ mia_cassetta = msx.Cassetta()
 try:
     mia_cassetta.load(os.getcwd() + "/tapes/msx_computer_magazine_06.cas")
     # mia_cassetta.load(os.getcwd() + "/tapes/ROADF.CAS")
-	# mia_cassetta.load(os.getcwd() + "/tapes/Shamus.cas")
-	# mia_cassetta.load(os.getcwd() + "/tapes/NV08B.CAS")
+    # mia_cassetta.load(os.getcwd() + "/tapes/Shamus.cas")
+    # mia_cassetta.load(os.getcwd() + "/tapes/NV08B.CAS")
 except msx.Eccezione as ex:
     print("Whoops... something went wrong:\n{}".format(ex))
 
